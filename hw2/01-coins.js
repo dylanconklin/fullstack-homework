72364fa238b5ce94581fe30dc7495d92a6466c67
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-04-27 14:42:00 -0700

    Finished HW2

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -2,6 +2,62 @@
 
 // Add your function here 
 
+class Change {
+    dollars = 0;
+    quarters = 0;
+    dimes = 0;
+    nickels = 0;
+    pennies = 0;
+
+    toString = function toStringDefinition() {
+        return [
+            [this.dollars, "dollar", "dollars"],
+            [this.quarters, "quarter", "quarters"],
+            [this.dimes, "dime", "dimes"],
+            [this.nickels, "nickel", "nickels"],
+            [this.pennies, "penny", "pennies"],
+        ]
+            .filter((e) => e[0] > 0)
+            .map((e) => e[0] === 1 ? [e[0], e[1]] : [e[0], e[2]])
+            .map((e) => e.join(" "))
+            .join(", ");
+    }
+
+    constructor(change) {
+        this.dollars = Math.trunc(change)
+        change = Math.trunc(Math.round((change - this.dollars) * 100))
+        while (change >= 25) {
+            change -= 25;
+            this.quarters += 1;
+        }
+        while (change >= 10) {
+            change -= 10;
+            this.dimes += 1;
+        }
+        while (change >= 5) {
+            change -= 5;
+            this.nickels += 1;
+        }
+        this.pennies = change;
+    }
+}
+
+let calculateChange = function calculateChangeDefinition(change = 0) {
+    let result = "";
+    if (change < 0) {
+        result = "Error: the number is negative";
+    } else if (change > 100) {
+        result = "Error: the number is too large";
+    } else {
+        let changeObj = new Change(change);
+        result = changeObj.toString();
+        if (result.length === 0) {
+            result = "No change";
+        }
+    }
+    return result;
+}
+
 // Sample test cases
 console.log(calculateChange(4.62));
 // $4.62 ==> 4 dollars, 2 quarters, 1 dime, 2 pennies
@@ -9,5 +65,17 @@ console.log(calculateChange(0.16));
 // $0.16 ==> 1 dime, 1 nickel, 1 penny
 console.log(calculateChange(150.11));
 // $150.11 ==> Error: the number is too large
-
-// Add additional test cases here 
+console.log(calculateChange());
+// EMPTY ==> No change
+console.log(calculateChange(-0.01));
+// $-0.01 ==> Error: the number is negative
+console.log(calculateChange(0));
+// $0 ==> No change
+console.log(calculateChange(0.01));
+// $0.01 ==> 1 penny
+console.log(calculateChange(99.99));
+// $99.99 ==> 99 dollars, 3 quarters, 2 dimes, 4 pennies
+console.log(calculateChange(100.00));
+// $100.00 ==> 100 dollars
+console.log(calculateChange(100.01));
+// $150.11 ==> Error: the number is too large
--- hw2/04-form.js
+++ hw2/04-form.js
@@ -1,5 +1,32 @@
+const express = require('express');
+const app = express();
+const path = require('path');
 const port = process.env.PORT || 5001;
 
 // http://localhost:5001/form should return a form with input elements for username, email, and submit button
 
 // http://localhost:5001/submit should return all the data the user entered
+
+const routes = [
+    'form',
+    'submit'
+];
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/form', (req, res) => {
+    res.status(200).type('html').sendFile(path.join(__dirname, "form", 'index.html'));
+});
+
+app.post('/submit', (req, res) => {
+    res.status(200).type('html').send(
+        [
+            '<p>Name: ' + `${req.body.name}` + '</p>',
+            '<p>Email: ' + `${req.body.email}` + '</p>',
+        ].join("")
+    );
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});

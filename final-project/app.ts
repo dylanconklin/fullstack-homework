d1f95405134195c4bce60eab37f2c2f7d633af56
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-06-07 17:04:23 -0700

    Increased Feed Size in What's Hot

--- final-project/app.ts
+++ final-project/app.ts
@@ -31,7 +31,7 @@ app.get("/whats-hot", async (req, res) => {
   });
   const { data } = await agent.app.bsky.feed.getFeed({
     feed: "at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.generator/whats-hot",
-    limit: 3,
+    limit: 100,
   });
   const { feed: postsArray, cursor: nextPage } = data;
   const feed = postsArray.map((e) => new Post(agent, e));
